3ff97bdcc691e9925de3ee21bcedce0e69860df3
Cesar CORNEJO <cesar.cornejo@geodis.com>
2020-05-15 16:52:05 -0500

    Get firebase expenses on start

--- src/actions/expenses.js
+++ src/actions/expenses.js
@@ -39,3 +39,25 @@ export const editExpense = (id, updates) => ({
     id,
     updates
 });
+
+export const setExpenses = (expenses) => ({
+    type: 'SET_EXPENSES',
+    expenses
+});
+
+export const startSetExpenses = () => {
+    return (dispatch) => {
+        return database.ref('expenses').once('value').then((ss) => {
+            const expenses = [];
+
+            ss.forEach((childSS) => {
+                expenses.push({
+                    id: childSS.key,
+                    ...childSS.val()
+                });
+            });
+
+            dispatch(setExpenses(expenses));
+        });
+    };
+};
